798809e14105befa381fa24761374254070b4b15
Márcio Sousa <msjdesigner@gmail.com>
2022-11-10 11:21:38 -0300

    deploy-12

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
-
-sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
-
-sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/local/bin/pm2"
-
 git fetch --all
 git pull origin master
 pm2 start index.js
