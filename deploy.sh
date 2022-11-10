676a6f48b109904e79a69697f11437680e87d5bb
Márcio Sousa <msjdesigner@gmail.com>
2022-11-10 15:04:46 -0300

    deploy-26

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 pwd
 # git fetch --all
 # git pull origin master
-sudo npm install pm2@latest -g
+cd ~/home/ubuntu/node-app
+# sudo npm install pm2@latest -g
 pm2 start index.js
 # pm2 restart 0 --update-env
